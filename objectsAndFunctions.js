5a5a9d3aadde24dd49789dc5fc246d701f137381
Vlad Golovchik <golo.vv@yandex.ru>
2022-07-02 00:32:55 +0300

    Fix after review
    Formatted the code, corrected grammatical errors, corrected the checkPrimeNumber function.

--- objectsAndFunctions.js
+++ objectsAndFunctions.js
@@ -1,34 +1,33 @@
-//-        Create a car object, add a color property to it with the value equals 'black'
+//-Create a car object, add a color property to it with the value equals 'black'
 let car = {
     color: 'black',
 };
 
-//-        Change the color property of the car object to 'green'
+//-Change the color property of the car object to 'green'
 car.color = 'green';
-car["color"] = 'green';
+//car["color"] = 'green';
 
-//-        Add the power property to the car object, which is a function and displays the engine power to the console
+//-Add the power property to the car object, which is a function and displays the engine power to the console
 car.power = function () {
     console.log("engine power...");
 };
 
 car.power();
 
-//-        Pears and apples are accepted to the warehouse, write a function that returns 
-//   the result of adding the number of accepted pears and apples
-function calcSumPearsAndApples(nomberOfPears, nomberOfApples) {
-    if (typeof nomberOfPears == "number" && typeof nomberOfApples == "number") {
-        return nomberOfPears + nomberOfApples;
+// -Pears and apples are accepted to the warehouse, write a function that returns 
+//the result of adding the number of accepted pears and apples
+function calcSumPearsAndApples(numberOfPears, nomberOfApples) {
+    if (typeof numberOfPears == "number" && typeof nomberOfApples == "number") {
+        return numberOfPears + nomberOfApples;
     }
-    
     return "Type of patameters must be a 'number'!!!";
 }
 
 console.log(calcSumPearsAndApples(5, 7));
 console.log(calcSumPearsAndApples("е", 7));
 
-//-       Your name is saved in the payment terminal, write a function to define the name in the terminal
-//   (if you entered your name, then hello + name, if not, then there is no such name)
+// -Your name is saved in the payment terminal, write a function to define the name in the terminal
+//(if you entered your name, then hello + name, if not, then there is no such name)
 let terminal = {
     name: "Uladzislau",
 };
@@ -44,7 +43,7 @@ function authorizationInTheTerminal(name) {
 console.log(authorizationInTheTerminal("Uladzislau"));
 console.log(authorizationInTheTerminal("Alex"));
 
-//-        Write a function for calculating the type of argument and type output to the console
+//-Write a function for calculating the type of argument and type output to the console
 function showTypeofArfument(arg) {
     type = typeof arg;
 
@@ -55,26 +54,24 @@ showTypeofArfument(5);
 showTypeofArfument("33");
 showTypeofArfument(true);
 
-//-        Write a function that determines whether a number is prime or not
-function checkPrimeNumber(n) {
-
-    if (typeof n != "number" || n % 1 != 0) {
+//-Write a function that determines whether a number is prime or not
+function checkPrimeNumber(number) {
+    if (typeof number !== "number" || number < 2 ||  !Number.isInteger(number)) {
         return false;
     }
 
-    if (n <= 3) {
-       return true; 
+    for (let index = 2; index < number; index++) {
+        if(number % index === 0) {
+            return false;
+        }    
     }
-
-    if (n % 2 != 0 && n % 3 != 0) {
-        return true;
-    }
-    
-    return false;
+    return true;
   }
 
+  console.log(checkPrimeNumber(1));
   console.log(checkPrimeNumber(3));
   console.log(checkPrimeNumber(4));
   console.log(checkPrimeNumber(7));
   console.log(checkPrimeNumber("9"));
   console.log(checkPrimeNumber(11.1));
+  console.log(checkPrimeNumber(23));
